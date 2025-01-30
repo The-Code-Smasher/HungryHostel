7bd527e0e04f017b482ead3420ad6b128c044214
karan chauhan <karan952899@gmail.com>
2025-01-30 11:53:05 +0530

    Navbar created and payment gateway working no

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react';
+import { CartProvider } from './context/CardContext';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CustomerDashboard from './Pages/CustomerDashboard';
-import './App.css'
+import './App.css';
+import Payment from './Components/payment/payment';
 
 function App() {
     return (
-        <>
-            <CustomerDashboard/>
-        </>
+        <Router>
+            <CartProvider>
+                <Routes>
+                <Route path="/" element={<CustomerDashboard />} />
+                <Route path="/payment" element={<Payment />} />
+                </Routes>
+            </CartProvider>
+        </Router>
     )
 }
 
--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaCircle, FaSearch } from "react-icons/fa";
+import "./Navbar.css";
+import { useCart } from "../context/CardContext";
+import Logo from "../assets/logo.png";
+import Basket from "../assets/purchase.png";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+    const { getTotalCount } = useCart();
+    const totalItems = getTotalCount();
+    const [isCartOpen, setIsCartOpen] = useState(false);
 
-export default Navbar
+    const handleCartClick = () => {
+        setIsCartOpen(!isCartOpen);
+    };
+
+    return (
+        <>
+            <div className="navbar">
+                <div className="logo">
+                    <img src={Logo} alt="logo" width={100} />
+                </div>
+                <div className="search">
+                    <input type="search" placeholder="Search" />
+                    <button>
+                        <FaSearch />
+                    </button>
+                </div>
+                <div>
+                    <span className="shopping-bsk cart-icon" onClick={handleCartClick}>
+                        <img src={Basket} alt="Basket" height={50} width={50} />
+                        {totalItems > 0 && <span className="cart-count">{totalItems}</span>}
+                    </span>
+                    <span className="profile">
+                        <FaCircle />
+                    </span>
+                </div>
+            </div>
+
+            {/* Cart popup appears when isCartOpen is true */}
+            {isCartOpen && <Cart onClose={() => setIsCartOpen(false)} />}
+        </>
+    );
+};
+
+export default Navbar;
