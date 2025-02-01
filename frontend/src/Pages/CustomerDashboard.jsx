6974048781b7ef58fca2437de769d42c0afd9677
SakshiCA088 <jsakshi.2707@gmail.com>
2025-02-01 23:11:06 +0530

    Slider work done

--- frontend/src/Pages/CustomerDashboard.jsx
+++ frontend/src/Pages/CustomerDashboard.jsx
@@ -5,12 +5,19 @@ import CategoryMenu from "../Components/CategoryMenu";
 import "./CustomerDashBoard.css";
 import FoodItems from "../Components/FoodItems";
 import FeatureFoodSlider from "../Components/FeatureFoodSilder";
+// import ToggleSearch from "../Components/ToggleSearch";
 
 const CustomerDashBoard = () => {
     return(
         <>
+            {/* //<ToggleSearch /> */}
             <Navbar />
-            <div className="home-container" style={{ backgroundImage: `url(${item1})`}}></div>
+            <div className="home-container" style={{ backgroundImage: `url(${item1})`}}>
+                <div className="dashboard-content">
+                    <h1 className="hungryhst">HUNGRY HOSTEL</h1>
+                    <h4 className="slogan">SATISY YOUR CRAVING WITHIN CAMPUS !</h4>
+                </div>
+            </div>
             <FeatureFoodSlider/>
             <CategoryMenu />
             <FoodItems />
