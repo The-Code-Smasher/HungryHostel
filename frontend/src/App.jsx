72ea59e3b8a0d87c9ffe6df7e0c949388b16c524
SakshiCA088 <jsakshi.2707@gmail.com>
2025-02-03 02:21:16 +0530

    Restaurant Owner page linked

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { CartProvider } from './context/CardContext';
 //import ProtectedRoute from './ProtectedRoute';
+import RestaurantDashboard from './Pages/ResturantDashboard';
 import CustomerDashboard from './Pages/CustomerDashboard';
 import Payment from './Components/payment/payment';
 import Login from './Components/Auth/login';
@@ -21,6 +22,7 @@ function App() {
                     />
                     <Route path="/payment" element={<Payment />} />
                     <Route path="/login" element={<Login />} />
+                    <Route path="/resturant" element={<RestaurantDashboard />} />
                     <Route path="/register" element={<Register />} />
                 </Routes>
             </CartProvider>
