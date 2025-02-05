c1253d8f8150779019f0022fe4df54ed0a19d38a
SakshiCA088 <jsakshi.2707@gmail.com>
2025-02-05 19:24:57 +0530

    home page sliding images done

--- frontend/src/Pages/CustomerDashboard.jsx
+++ frontend/src/Pages/CustomerDashboard.jsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../Components/Navbar";
-import item1 from '../assets/item1.png';
 import CategoryMenu from "../Components/CategoryMenu";
 import "./CustomerDashBoard.css";
 import FoodItems from "../Components/FoodItems";
@@ -8,20 +7,38 @@ import FeatureFoodSlider from "../Components/FeatureFoodSilder";
 import ToggleSearch from "../Components/ToggleSearch";
 import { FaSearch } from "react-icons/fa";
 
+import img1 from "../assets/slider1.jpg";
+import img2 from "../assets/slider2.jpg";
+import img3 from "../assets/slider3.jpg";
+
+const slideImages = [img1, img2, img3];
+
 const CustomerDashBoard = () => {
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((prevIndex) => (prevIndex + 1) % slideImages.length);
+        }, 2000); // 2 seconds per image
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <>
-            
             <ToggleSearch />
             <Navbar />
-            <div className="home-container" style={{ backgroundImage: `url(${item1})` }}>
+            <div
+                className="home-container"
+                style={{ backgroundImage: `url(${slideImages[index]})` }}
+            >
                 <div className="dashboard-content">
                     <h1 className="hungryhst">HUNGRY HOSTEL</h1>
                     <h4 className="slogan">SATISFY YOUR CRAVING WITHIN CAMPUS!</h4>
-                    {/* Search Bar */}
+
                     <div className="dashboard-search">
                         <input className="search" type="search" placeholder="Search food items..." />
-                        <button > 
+                        <button>
                             <FaSearch />
                         </button>
                     </div>
@@ -35,3 +52,6 @@ const CustomerDashBoard = () => {
 };
 
 export default CustomerDashBoard;
+
+
+
